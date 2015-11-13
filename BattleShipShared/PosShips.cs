7459718340c123a701abe1ad45e63e0d178b390b
Francis <super_f_qc@hotmail.com>
2015-11-13 15:15:14 -0500

    Commentaire PosShips

--- BattleShipShared/PosShips.cs
+++ BattleShipShared/PosShips.cs
@@ -17,18 +17,24 @@ namespace BattleShipShared
                 Verticale,
                 Horizontale
             }
+
+            //Infos Porte-Avion
             public Point PPorteAvion;
             public Orientation OPorteAvion;
 
+            //Infos Croiseur
             public Point PCroiseur;
             public Orientation OCroiseur;
 
+            //Infos Contre-Torpilleur
             public Point PContreTorpilleur;
             public Orientation OContreTorpilleur;
 
+            //Infos Sous-Marin
             public Point PSousMarin;
             public Orientation OSousMarin;
 
+            //Infos Torpilleur
             public Point PTorpilleur;
             public Orientation OTorpilleur;
 
