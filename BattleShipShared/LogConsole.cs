26ef5a2426d9f8a10da646b9eaf93f4c372e6258
Francis <super_f_qc@hotmail.com>
2015-11-13 15:12:21 -0500

    Commentaire LogConsole

--- BattleShipShared/LogConsole.cs
+++ BattleShipShared/LogConsole.cs
@@ -10,10 +10,19 @@ namespace BattleShipShared
     {
         public class LogConsole
         {
+            /// <summary>
+            /// Écrit dans la console
+            /// </summary>
+            /// <param name="Message"></param>
             public static void Log(String Message)
             {
                 Console.WriteLine(">> " + Message);
             }
+
+            /// <summary>
+            /// Écrit dans la console en y indiquant la date et l'heure
+            /// </summary>
+            /// <param name="Message"></param>
             public static void LogWithTime(String Message)
             {
                 Console.WriteLine(DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + ">> " + Message);
